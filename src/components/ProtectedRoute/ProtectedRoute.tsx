a5b437e6e31daeaa0dc34160d83bd8f07dccf646
Wojciech Kurzak <wojciechkurzak@sitero.net>
2023-10-13 23:27:37 +0200

    Fix passing unauthorized user

--- src/components/ProtectedRoute/ProtectedRoute.tsx
+++ src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -2,16 +2,17 @@ import { useAppSelector } from '../../redux/hooks'
 import { ProtectedRouteType } from './protected-route-type'
 import { useNavigate } from 'react-router-dom'
 import { useEffect } from 'react'
+import Loading from '../Loading/Loading'
 
 const ProtectedRoute = ({ component }: ProtectedRouteType) => {
   const navigate = useNavigate()
-  const isAuth = useAppSelector((state) => state.auth)
+  const isAuth = useAppSelector((state) => state.auth.uid)
 
   useEffect(() => {
     !isAuth && navigate('/login', { replace: true })
   }, [])
 
-  return component
+  return <>{isAuth ? component : <Loading />}</>
 }
 
 export default ProtectedRoute
